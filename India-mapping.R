4fb7d68323eb49519197a61b94a05b0be2ee7a9d
preetrajdeo <preetrajdeo@gmail.com>
2017-10-18 15:28:34 -0400

    initial commit

--- India-mapping.R
+++ India-mapping.R
@@ -3,131 +3,92 @@ library(maps)
 library(maptools)
 library(rgdal)
 
-# Conterminous United States
-map("state")
-map("state", regions = "california")
-map("state", regions = "washington")
-map("state", regions = "florida")
-# States together
-map("state", regions = c("california", "washington", "florida"))
-# See state region names
-state.fips
-
-#Working with Shapefiles
-
-usa <- readShapePoly("data/census_bureau/cb_2013_us_state_20m/cb_2013_us_state_20m.shp")
-plot(usa)
-# Only conterminous United States
-conterm <- usa[usa$STATEFP != "02" & usa$STATEFP != "15" & usa$STATEFP != "72",]
-plot(conterm)
-# World
-countries <- readShapePoly("data/nautral_earth/ne_110m_admin_0_countries/ne_110m_admin_0_countries.shp")
-plot(countries)
-# Spain
-spain <- countries[countries$name == "Spain",]
-plot(spain)
-# India
-india <- countries[countries$name == "India",]
-plot(india)
-# Canada
-canada <- countries[countries$name == "Canada",]
-plot(canada)
-# See country names
-countries$name
-countries@data[1:2, 1:5]
-colnames(countries@data)
-# Europe
-india_proj4 <- "+proj=aea +lat_1=28 +lat_2=12 +lat_0=20 +lon_0=78 +x_0=2000000 +y_0=2000000 +ellps=WGS84 +datum=WGS84 +units=m +no_defs"
-proj4string(countries) <- CRS("+proj=longlat")
-india <- countries[countries$name == "India",]
-india_projected <- spTransform(india, CRS(india_proj4))
-plot(india_projected, asp=1)
-install.packages("nycflights13")
-library(nycflights13)
-ind<- readShapePoly("IND_adm/IND_adm2")
-india_proj4 <- "+proj=aea +lat_1=28 +lat_2=12 +lat_0=20 +lon_0=78 +x_0=2000000 +y_0=2000000 +ellps=WGS84 +datum=WGS84 +units=m +no_defs"
-plot(ind)
-
-# US Boundaries
-usa <- readShapePoly("data/census_bureau/cb_2013_us_state_20m/cb_2013_us_state_20m.shp", proj4string=CRS("+proj=longlat"))
-conterm <- usa[usa$STATEFP != "02" & usa$STATEFP != "15" & usa$STATEFP != "72",]
-plot(conterm)
-
-# Points on the map, non-projected
-points(airports$lon, airports$lat)
-
-# Project
-conterm_proj4 <- "+proj=aea +lat_1=29.5 +lat_2=45.5 +lon_0=97.2w"
-conterm_projected <- spTransform(conterm, CRS(conterm_proj4))
-
-plot(conterm_projected)
-latlon <- data.frame(airports$lon, airports$lat)
-coordinates(latlon) <- c("airports.lon", "airports.lat")
-
-proj4string(latlon) <- CRS("+proj=longlat")
-latlon_projected <- spTransform(latlon, CRS(conterm_proj4))
-points(latlon_projected)
-plot(conterm)
-
-symbols(airports$lon, airports$lat, circles=.008*sqrt(airports$alt), 
-        add=TRUE, inches=FALSE, bg="#00000050", lwd=0.5)
-
-plot(conterm_projected, border="#cccccc", lwd=0.5)
-
-symbols(latlon_projected@coords, circles=800*sqrt(airports$alt), 
-        add=TRUE, inches=FALSE, bg="#00000050", lwd=0.5)
-crime <- read.csv("http://datasets.flowingdata.com/crimeRatesByState2005.tsv", header=TRUE, sep="\t")
-
-View(crime)
-
-lax <- airports[airports$faa =="LAX",]
-jfk <- airports[airports$faa =="JFK",]
-
-plot(conterm, border="#cccccc", lwd=0.5)
-
-points(c(lax$lon, jfk$lon), c(lax$lat, jfk$lat), col="red", lwd=2, pch=19)
-
-states <- readShapePoly("data/census_bureau/cb_2013_us_state_20m/cb_2013_us_state_20m.shp", proj4string=CRS("+proj=longlat"))
-
-plot(ind)
-data <- read.csv("nfhs4-district-level.csv")
-
 data <-read.csv("data.csv")
-data_women <- aggregate(Women~State.1, data=data, FUN=sum)
-data_men <- aggregate(Men~State.1, data=data, FUN=sum)
-total <- merge(data_women,data_men,by="State.1")
-total$percent_women <- total$Women/(total$Women + total$Men)
+data$ImprovedDrinkingWater<-data$Households.with.an.improved.drinking.water.source1
+data$ImprovedDrinkingWater<-data$ImprovedDrinkingWater/100
+#data_women <- aggregate(Women~State.1, data=data, FUN=sum)
+#data_men <- aggregate(Men~State.1, data=data, FUN=sum)
+data1 <- aggregate(ImprovedDrinkingWater~State.1, data=data, FUN=mean)
+
+#total <- merge(data_women,data_men,by="State.1")
+#total$percent_women <- total$Women/(total$Women + total$Men)
 ind<- readShapePoly("IND_adm/IND_adm1", 
                     proj4string =CRS("+proj=aea +lat_1=28 +lat_2=12 +lat_0=20 +lon_0=78 +x_0=2000000 +y_0=2000000 +ellps=WGS84 +datum=WGS84 +units=m +no_defs"))
 pickColor <- function(x) {
   if (is.na(x)) {
     col <- "#ffffff"
   }
-  else if (x > .1) {
-    col <- "#636363"
+  else if (x < 0.5) {
+    col <- "#f0f0f0"
   }
-  else if (x > .05) {
+  else if (x < 0.6) {
+    col <- "#cecece"
+  }
+  else if (x < 0.7) {
     col <- "#bdbdbd"
   }
+  else if (x < .8) {
+    col <- "#a5a5a5"
+  }
+  else if (x < .9) {
+    col <- "#939393"
+  }
   else {
-    col <- "#f0f0f0"
+    col <- "#636363"
   }
   return(col)
 }
-total$col <- sapply(total$percent_women, pickColor)
 
-plot(ind, col=total$col, border="#ffffff", lwd=0.5)
+data1$col <- sapply(data1$ImprovedDrinkingWater, pickColor)
+
+plot(ind, col=data1$col, border="#ffffff", lwd=0.5)
 
 par(new=TRUE, plt=c(0.1, 0.25, .35, .5))
 plot(0:1, 0:1, type="n", xlab="", ylab="", asp=1, axes=FALSE)
-col <- c("#f0f0f0", "#bdbdbd", "#636363")
-breaks <- c(2, 3)
+col <- c("#ffffff", "#f0f0f0", "#cecece", "#bdbdbd", "#a5a5a5","#939393", "#636363")
+breaks <- c(2, 3,4,5,6,7)
 rect_width <- 1 / length(col)
 xleft <- 0:(length(col)-1) * rect_width
 ybottom <- rep(.1, length(col))
 xright <- 1:length(col) * rect_width
 ytop <- rep(.2, length(col))
 rect(xleft, ybottom, xright, ytop, col=col, border="#e0e0e0", lwd=.5)
-text(.5, .35, "Births Per Woman\n in 2010", cex=.7)
+text(.5, .35, "% of Households With Improved Drinking Water", cex=0.7)
 text(1:(length(col)-1) * rect_width, .04, labels = breaks, cex=0.7)
 
+#States and Districts
+#Andaman and Nicobar
+ind_district<- readShapePoly("IND_adm/IND_adm2")
+ind_andamanandnicobar <- ind_district[ind_district$NAME_1 == "Andaman and Nicobar",]
+data$col <- sapply(data$ImprovedDrinkingWater, pickColor)
+plot(ind_andamanandnicobar, col=data$col, border="#ffffff", lwd=0.5, main = "Andaman and Nicobar")
+
+#Andhra Pradesh
+ind_district<- readShapePoly("IND_adm/IND_adm2")
+ind_andhrapradesh <- ind_district[ind_district$NAME_1 == "Andhra Pradesh",]
+data$col <- sapply(data$ImprovedDrinkingWater, pickColor)
+plot(ind_andhrapradesh, col=data$col, border="#ffffff", lwd=0.5, main = "Andhra Pradesh")
+
+#Arunachal Pradesh
+ind_district<- readShapePoly("IND_adm/IND_adm2")
+ind_arunachalpradesh <- ind_district[ind_district$NAME_1 == "Arunachal Pradesh",]
+data$col <- sapply(data$ImprovedDrinkingWater, pickColor)
+plot(ind_arunachalpradesh, col=data$col, border="#ffffff", lwd=0.5, main = "Arunachal Pradesh")
+
+#Assam
+ind_district<- readShapePoly("IND_adm/IND_adm2")
+ind_assam <- ind_district[ind_district$NAME_1 == "Assam",]
+data$col <- sapply(data$ImprovedDrinkingWater, pickColor)
+plot(ind_assam, col=data$col, border="#ffffff", lwd=0.5, main = "Assam")
+
+#Assam
+ind_district<- readShapePoly("IND_adm/IND_adm2")
+ind_bihar <- ind_district[ind_district$NAME_1 == "Bihar",]
+data$col <- sapply(data$ImprovedDrinkingWater, pickColor)
+plot(ind_bihar, col=data$col, border="#ffffff", lwd=0.5, main = "Bihar")
+
+#Karnataka
+ind_district<- readShapePoly("IND_adm/IND_adm2")
+ind_karnataka <- ind_district[ind_district$NAME_1 == "Karnataka",]
+data$col <- sapply(data$ImprovedDrinkingWater, pickColor)
+plot(ind_karnataka, col=data$col, border="#ffffff", lwd=0.5, main = "Karnataka")
